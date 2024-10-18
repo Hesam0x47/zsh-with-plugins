b46dd1a3f1c0ba62c003dfa79d3aa2b0ff50a88a
Hessam Ghassemi <44605128+Hesam0x47@users.noreply.github.com>
2024-10-18 14:29:45 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,3 +8,7 @@ sudo apt update && sudo apt install -y zsh curl
 # Install Oh My Zsh
 echo "Installing Oh My Zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+
+# Set Zsh as the default shell for the user
+echo "Setting Zsh as the default shell..."
+chsh -s "$(which zsh)"
