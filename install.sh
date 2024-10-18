a9a1ed2143a43ff01ab078e349b975ca4d66fc1b
Hessam Ghassemi <44605128+Hesam0x47@users.noreply.github.com>
2024-10-18 14:25:04 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,5 +8,3 @@ sudo apt update && sudo apt install -y zsh curl
 # Install Oh My Zsh
 echo "Installing Oh My Zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Hesam0x47/zsh-with-plugins/refs/heads/main/install-plugins-and-others.sh)" --unattended
