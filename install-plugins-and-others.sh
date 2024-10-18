a1d7ef97847fb23a941f37f7789e35cd4dd98db3
Hessam Ghassemi <44605128+Hesam0x47@users.noreply.github.com>
2024-10-18 15:56:22 +0330

    Update install-plugins-and-others.sh

--- install-plugins-and-others.sh
+++ install-plugins-and-others.sh
@@ -1,19 +1,24 @@
+#!/usr/bin/zsh
+
+# Set the ZSH_CUSTOM variable
+export ZSH_CUSTOM="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"
+
 # Install zsh-autosuggestions
 echo "Installing zsh-autosuggestions..."
-git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
 
 # Install zsh-syntax-highlighting
 echo "Installing zsh-syntax-highlighting..."
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
 
 # Install zsh-autocomplete
 echo "Installing zsh-autocomplete..."
-git clone https://github.com/marlonrichert/zsh-autocomplete "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete"
+git clone https://github.com/marlonrichert/zsh-autocomplete "$ZSH_CUSTOM/plugins/zsh-autocomplete"
 
 # Enable the plugins in the ~/.zshrc file
 echo "Enabling plugins in .zshrc..."
 sed -i '/plugins=(git)/c\plugins=(git zsh-autosuggestions zsh-syntax-highlighting zsh-autocomplete)' ~/.zshrc
-echo 'source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete/zsh-autocomplete.plugin.zsh' >> ~/.zshrc
+echo 'source $ZSH_CUSTOM/plugins/zsh-autocomplete/zsh-autocomplete.plugin.zsh' >> ~/.zshrc
 
 # Source the ~/.zshrc file to apply the changes
 echo "Sourcing .zshrc to apply changes..."
